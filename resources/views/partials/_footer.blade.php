ef7eb6659f3b1555137d6a85d2bfaadb27b4d3dc
TiagoPerdigao1 <dw.angelx@gmail.com>
2018-06-27 00:36:09 +0100

    Imagens do footer horizontalmente

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -5,16 +5,16 @@
 	<div class="container">
 
     <div class="row">
-    	<h2>Entidades Apoiantes</h2>
-    	<div class="column">
+    	<h2 class="col-sm-12">Entidades Apoiantes</h2>
+    	<div class="col-sm-12">
     		<a href="http://ipvc.pt">
 	    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:30px">
 	    	</a>
+	    	<a href="http://ipvc.pt">
+	    		<img src="{{ asset('uploads/IndvDef.png') }}" alt="Snow" style="width:30px">
+	    	</a>
  		</div>
-  		<div class="column">
-    		<img src="img_forest.jpg" alt="Forest" style="width:100%">
-  		</div>
-  		<div class="column">
+  		<div class="col-sm-4">
    			<img src="img_mountains.jpg" alt="Mountains" style="width:100%">
  		</div>
         <div class="col-lg-12">
