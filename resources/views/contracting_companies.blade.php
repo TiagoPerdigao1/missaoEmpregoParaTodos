246b6cc3dc436aae6c7e425bc6cbf4a11b62d63f
Tiago Cepa <tgcepa4@gmail.com>
2018-06-26 12:07:30 +0100

    alterações +

--- resources/views/contracting_companies.blade.php
+++ resources/views/contracting_companies.blade.php
@@ -8,7 +8,7 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Empresas Contratantes
+                <h1 class="page-header">Empregadoras
                     <small>Subheading</small>
                 </h1>
             </div>
--- resources/views/support_entities.blade.php
+++ resources/views/support_entities.blade.php
@@ -8,7 +8,7 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Entidades de Apoio
+                <h1 class="page-header">Apoiantes
                     <small>Subheading</small>
                 </h1>
             </div>
