ab010b12d780f29208b71be048215957a6731c38
Tiago Cepa <tgcepa4@gmail.com>
2018-06-26 12:32:31 +0100

    alter

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -5,7 +5,16 @@
 	<div class="container">
 
     <div class="row">
-    	<p>Entidades Apoiantes</p>
+    	<h2>Entidades Apoiantes</h2>
+    	<div class="column">
+    		<img src="img_snow.jpg" alt="Snow" style="width:100%">
+ 		</div>
+  		<div class="column">
+    		<img src="img_forest.jpg" alt="Forest" style="width:100%">
+  		</div>
+  		<div class="column">
+   			<img src="img_mountains.jpg" alt="Mountains" style="width:100%">
+ 		</div>
         <div class="col-lg-12">
             <p>Copyright &copy; Missão Emprego Para Todos 2018</p>
         </div>
