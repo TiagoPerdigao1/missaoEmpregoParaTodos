048515e60808a953ac69c18814a98b7ea4f9439b
TiagoPerdigao1 <dw.angelx@gmail.com>
2018-07-03 01:11:49 +0100

    footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,25 +1,99 @@
 <hr>
 
 <!-- Footer -->
-<footer style="background: silver;">
+<footer style="background: lightgrey;">
 	<div class="container">
 
-    <div class="row">
-    	<h2 class="col-sm-12">Entidades Apoiantes</h2>
-    	<div class="col-sm-12">
-    		<a href="http://ipvc.pt">
-	    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:30px">
-	    	</a>
-	    	<a href="http://ipvc.pt">
-	    		<img src="{{ asset('uploads/IndvDef.png') }}" alt="Snow" style="width:30px">
-	    	</a>
- 		</div>
-  		<div class="col-sm-4">
-   			<img src="img_mountains.jpg" alt="Mountains" style="width:100%">
- 		</div>
-        <div class="col-lg-12">
-            <p>Copyright &copy; Missão Emprego Para Todos 2018</p>
-        </div>
-    </div>
+	    <div class="row">
+	    	<div class="col-sm-6">
+		    	<h6 class="col-sm-12">Entidades Apoiantes</h6>
+		    	<div class="col-sm-12">
+		    		<p></p>
+		    		<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/IndvDef.png') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<p></p>
+		 		</div>
+		  		<h6 class="col-sm-12">Entidades Promotoras</h6>
+		  		<div class="col-sm-12">
+		  			<p></p>
+		    		<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/IndvDef.png') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<a href="http://ipvc.pt">
+			    		<img src="{{ asset('uploads/29893675_2095736613776842_927971633_o.jpg') }}" alt="Snow" style="width:60px">
+			    	</a>
+			    	<p></p>
+		 		</div>
+		        <h6 class="col-sm-12">Copyright &copy; Missão Emprego Para Todos 2018</h6>
+		    </div> 
+		    <!--Divisão ecrã-->   
+			<div class="col-sm-6">
+				<h6 class="col-sm-12">Contactos</h6>
+		    	
+		    	<div class="col-sm-12" style="color:blue;">
+			    	<p></p><p></p>
+			    	<p>Morada</p>	
+		 		</div>
+		 		<div class="col-sm-12" style="font-size:90%;">
+			    	<p>Estrada da Papanata, nº52</p>
+			    	<p>4900-470 Viana do Castelo</p>	
+		 		</div>
+
+		 		<div class="col-sm-12" style="color:blue;">
+			    	<p></p><p></p><p></p>
+			    	<p>Telemóvel</p>	
+		 		</div>
+
+		 		<div class="col-sm-12" style="font-size:90%;">
+			    	<p>+351 XXX XXX XXX</p>
+		 		</div>
+
+		 		<div class="col-sm-12" style="color:blue;">
+			    	<p></p><p></p><p></p>
+			    	<p>Telefone</p>	
+		 		</div>
+
+		 		<div class="col-sm-12" style="font-size:90%;">
+			    	<p>+351 XXX XXX XXX</p>
+		 		</div>
+
+		 		<div class="col-sm-12" style="color:blue;">
+			    	<p></p><p></p><p></p>
+			    	<p>Email</p>	
+		 		</div>
+
+		 		<div class="col-sm-12" style="font-size:90%;">
+			    	<p>geral@despertar.com.pt</p>
+		 		</div>
+
+
+			</div>    
+	    </div>
+	
 	</div>
 </footer>
